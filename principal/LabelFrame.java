cd74146073a4a8a44af61bff2f9dfcbaf70b1333
ericojonck <39502856+ericojonck@users.noreply.github.com>
2018-11-26 19:26:49 -0200

    adicionei JMenuBar
    precisa completar com alguns comandos

--- principal/LabelFrame.java
+++ principal/LabelFrame.java
@@ -1,6 +1,7 @@
 package menu.principal;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.net.*;
 import javax.swing.*;
 
@@ -15,6 +16,41 @@ public class LabelFrame extends JFrame {
     public LabelFrame() throws MalformedURLException {
         super("Main Menu");
         setLayout(new BorderLayout());
+        
+        //essa parte tem que ter em todas as telas do jogo
+        //fazer só quando tiver acabado pq não é necessário ter isso
+        
+        JMenu fileMenu = new JMenu("Arquivo");
+        fileMenu.setMnemonic('A');
+        JMenuItem novoItem = new JMenuItem("Novo");
+        novoItem.setMnemonic('N');
+        novoItem.addActionListener((ae) -> {
+            
+            //fazer com que comece um novo jogo
+        
+        });
+        fileMenu.add(novoItem);
+        JMenuItem sobreItem = new JMenuItem("Sobre");
+	sobreItem.setMnemonic('S');
+	fileMenu.add(sobreItem);
+	sobreItem.addActionListener((ActionEvent event) -> {
+            JLabel labelSobre =new JLabel(); // escrever aqui o 'sobre' do arquivo       
+            JOptionPane.showMessageDialog(null, labelSobre,"Sobre",JOptionPane.INFORMATION_MESSAGE);
+        });
+        JMenuItem sairItem = new JMenuItem("Sair");
+        sairItem.setMnemonic('X');
+        fileMenu.add(sairItem);
+        sairItem.addActionListener((ActionEvent event) -> {
+            System.exit(0);
+        });
+        
+        JMenu historicoMenu = new JMenu("Histórico");
+        historicoMenu.setMnemonic('H');  //fazer aqui aparecer o histórico dos pontos que serializamos
+                       
+        JMenuBar bar = new JMenuBar();
+        setJMenuBar(bar);
+	bar.add(fileMenu);
+        bar.add(historicoMenu);
         
         panel1 = new myCustomPanel(background);
         panel1.setLayout(new GridLayout());
