53ea8e171e0571466d17f2bba339e742b1f008e7
NevesFelipe <42079601+NevesFelipe@users.noreply.github.com>
2018-11-29 17:27:17 -0200

    Add files via upload

---
+++ mainMenu/backgroundAdapt.java
@@ -0,0 +1,36 @@
+package principal;
+
+import java.awt.*;
+import java.net.*;
+import javax.swing.*;
+
+public class backgroundAdapt extends JPanel {
+
+    private Image background;
+
+    public backgroundAdapt(URL path) {
+        this.background = new ImageIcon(path).getImage();
+        setOpaque(false);
+        setPreferredSize(new Dimension(getWidth(), getHeight()));
+    }
+
+    backgroundAdapt() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        g.drawImage(background, 0, 0, this);
+        super.paintComponent(g);
+    }
+
+    @Override
+    public int getWidth() {
+        return background.getWidth(this);
+    }
+
+    @Override
+    public int getHeight() {
+        return background.getHeight(this);
+    }
+}
