00e355ff715ed651d3cddb3deaf659b14f8bc871
Damian <vaz.damian@gmail.com>
2019-01-07 17:17:39 -0200

    saved from termination

---
+++ mainMenu/MainMenu/myCustomPanel.java
@@ -0,0 +1,42 @@
+package MainMenu;
+
+import java.awt.*;
+import java.net.*;
+import javax.swing.*;
+
+public class myCustomPanel extends JPanel 
+{
+
+    private Image background;
+
+    public myCustomPanel(URL path) 
+    {
+        this.background = new ImageIcon(path).getImage();
+        setOpaque(false);
+        setPreferredSize(new Dimension(getWidth(), getHeight()));
+    }
+
+    myCustomPanel() 
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) 
+    {
+        g.drawImage(background, 0, 0, this);
+        super.paintComponent(g);
+    }
+
+    @Override
+    public int getWidth() 
+    {
+        return background.getWidth(this);
+    }
+
+    @Override
+    public int getHeight() 
+    {
+        return background.getHeight(this);
+    }
+}
